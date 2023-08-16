c9b6c47327212d0271dece87043439b1d0e21c6e
nisarga hag <nisargahag@gmail.com>
2023-08-16 21:57:08 +0530

    First commit

--- src/main/java/com/airtable/driver/AsanaController.java
+++ src/main/java/com/airtable/driver/AsanaController.java
@@ -7,42 +7,40 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 @RestController
-@RequestMapping("/webhook")
+@RequestMapping("/api/webhook")
 public class AsanaController {
 
-	private final AirtableService airtableService;
-
 	@Autowired
-	public AsanaController(AirtableService airtableService) {
-		this.airtableService = airtableService;
-	}
+	private AirtableService airtableService;
+
+	public static String secret;
 
 	@PostMapping("/asana")
-	public ResponseEntity<String> handleAsanaWebhook(@RequestBody AsanaDto asana) {
-		try {
-			// Extract task details from the payload
-			String taskId = asana.getTaskId();
-			String name = asana.getName();
-			String assignee = asana.getAssignee();
-			LocalDate dueDate = asana.getDueDate();
-			String description = asana.getDescription();
+	public ResponseEntity<String> handleAsanaWebhook(HttpServletRequest request, @RequestBody String payload,
+			HttpServletResponse response) {
 
-			// Make API call to Airtable to create a new record
-			boolean success = airtableService.createRecord(taskId, name, assignee, dueDate, description);
-
-			if (success) {
-				return ResponseEntity.ok("Task data copied to Airtable successfully.");
-			} else {
-				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-						.body("Failed to copy task data to Airtable.");
-			}
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid payload received from Asana.");
+		if (request.getHeader("x-hook-secret") != null) {
+			secret = request.getHeader("x-hook-secret");
+			response.setHeader("x-hook-secret", secret);
+			response.setStatus(200);
+			System.out.println(secret);
+			System.out.println(payload);
+			return ResponseEntity.status(HttpStatus.OK).body(secret);
+		} else if (request.getHeader("x-hook-signature") != null) {
+			System.out.println(secret);
+			System.out.println(payload);
+			return ResponseEntity.status(HttpStatus.OK).body("webhook processed");
+		} else {
+			response.setStatus(404);
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Invalid request.");
 		}
-
 	}
 }
